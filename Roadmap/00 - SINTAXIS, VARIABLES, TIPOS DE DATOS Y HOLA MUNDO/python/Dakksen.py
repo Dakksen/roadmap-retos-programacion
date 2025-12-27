e190c4824dfd479a131bdfda547d45248c90d74a
Dakksen <117780408+Dakksen@users.noreply.github.com>
2025-12-27 15:35:27 -0500

    Create Dakksen.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Dakksen.py
@@ -0,0 +1,28 @@
+# https://python.org
+
+# Comentario en una línea
+"""Esto va a ser 
+un comentario en varias líneas
+"""
+
+''' otra forma de hacer comentarios en varias líneas '''
+
+my_variable = "Mi variable"
+my_constant = "Nuevo valor de variable" 
+"""No se pueden dar variables en python"""
+
+MY_CONSTANT2 = "Cualquiera" 
+
+"""Escribir el nombre de la variable en mayuscula se reconoce como convención de que se debe mantener el valor"""
+my_int = 1
+my_float = 5.5
+my_bool = True
+my_bool = False
+my_str = "toda cadena de texto"
+# en python solo hay esos tipos de datos, son 4
+print("¡Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_str))
